d134792ad1775d5ee5445eea2a0b96521143e91b
allan6757 <allanoomaina@gmail.com>
2025-09-26 04:14:41 +0300

    Fix gunicorn app instance

--- server/app.py
+++ server/app.py
@@ -45,8 +45,10 @@ def create_app():
     
     return app
 
+# Create app instance for gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     with app.app_context():
         db.create_all()
     port = int(os.environ.get('PORT', 5001))
